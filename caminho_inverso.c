4a476a712ee139ee3c358fb8959cc8e6eec9db1e
lightguy875 <luz875@gmail.com>
2018-05-15 23:23:11 -0300

    Modificações finais

--- caminho_inverso.c
+++ caminho_inverso.c
@@ -1,19 +1,28 @@
-/*
-* Primeiro Exercício em C
-* Autor: Danillo Neves Souza
-*/
+/********************************************
+* Primeiro Exercício em C					*
+*											*
+* Autores: Danillo Neves Souza - 14/0135839 *
+*		   Andrei Buslik	   - 11/0024702 *
+*          Yan Victor		   - 14/0033599 *
+*		   Gustavo Costa	   - 14/0142568 *
+*											*
+* Disciplina: Software Básico				*
+* Professor: Marcelo Ladeira				*
+*											*			
+* Data: 15/05/2018							*
+*********************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char alfabeto[91] = { 0 };
+char alfabeto[91] = { 0 }; /* Vetor global para simular a tabela ASCII Base91 */
 
-/*************************************************
-Função que recebe um caracter, procura o caracter 
-no array 'alfabeto' e retorna o índice do caracter 
-quando o encontra no array.
-**************************************************/
+/*****************************************************
+* Função que recebe um caracter, procura o caracter  *
+* no vetor 'alfabeto' e retorna o índice do caracter *
+* quando o encontra no vetor.						 *
+*****************************************************/
 
 int base91(char caracter)
 {
@@ -26,28 +35,28 @@ int base91(char caracter)
 	}
 }
 
-/**********************************************************
-Função que preenche o array 'alfabeto' com os caracteres 
-da tabela ASCII Base91 para que seja consultada nas funções 
-de codificação e decodificação.
-***********************************************************/
+/**************************************************************
+* Função que preenche o vetor 'alfabeto' com os caracteres    *
+* da tabela ASCII Base91 para que seja consultada nas funções *
+* de codificação e decodificação.							  *
+***************************************************************/
 
 void preencheAlfabeto()
 {
 		char* aux = malloc(sizeof(char));
 		int soma = 0;
 		int i;
-		for(i = 0; i <= 25; i++)
+		for(i = 0; i <= 25; i++)			/* Os índices de 0 à 25 repesentam as 26 letras do alfabeto em maiúsculo */
 		{
 			alfabeto[i] = 'A' + i;
 		}
-		for(i = 26; i <= 51; i++)
+		for(i = 26; i <= 51; i++)			/* Os índices de 26 à 51 repesentam as 26 letras do alfabeto em minúsculo */
 		{
 			alfabeto[i] = 'a' + soma;
 			soma++;
 		}
 		soma = 0;
-		for(i = 52; i <= 61; i++)
+		for(i = 52; i <= 61; i++)			/* Os índices de 52 à 61 repesentam os números de 0 à 9 */
 		{
 			alfabeto[i] = '0' + soma;
 			soma++;
@@ -87,11 +96,11 @@ void preencheAlfabeto()
 		free(aux);
 }
 
-/**********************************************
-Função que lê um arquivo texto, decodifica seu 
-conteúdo a partir da tabela ASCII Base91 e grava 
-o conteúdo decodificado em um arquivo binário.
-************************************************/
+/***************************************************
+* Função que lê um arquivo texto, decodifica seu   *
+* conteúdo a partir da tabela ASCII Base91 e grava *
+* o conteúdo decodificado em um arquivo binário.   *
+***************************************************/
 
 void decodeTxt()
 {
@@ -135,25 +144,21 @@ void decodeTxt()
     }
 
 	
-	fscanf(fp_txt, "%c", read);
-	//printf("\natual: %c\n", read[0]);
-	primeiro_asc = base91(read[0]);
-	fscanf(fp_txt, "%c", read);
-	//printf("\natual: %c\n", read[0]);
-	segundo_asc = base91(read[0]);
-    //printf("\nOLAAA VEII 1: %d e 2: %d\n", primeiro_asc, segundo_asc);
+	fscanf(fp_txt, "%c", read);						/* Leitura do primeiro caracter a ser decodificado no arquivo texto */
+	primeiro_asc = base91(read[0]);                 /* Procura o caracter no vetor que representa a tabela ASCII Base91 */
+	fscanf(fp_txt, "%c", read);						/* Leitura do segundo caracter a ser decodificado no arquivo texto */
+	segundo_asc = base91(read[0]);                  /* Procura o caracter no vetor que representa a tabela ASCII Base91 */
 	original = (91)*(primeiro_asc) + (segundo_asc); /* Obtém número original */
-	fprintf(fp_bin, "%x\n", original);	/* Imprime o número no arquivo binário */
+	fprintf(fp_bin, "%x\n", original);	            /* Imprime o número no arquivo binário */
 	
-	while(!feof(fp_txt))
+	while(!feof(fp_txt))							/* O processo anterior é repetido até que se chegue ao final do arquivo texto */
 	{
 	 fscanf(fp_txt, "%c", read); 
-	 primeiro_asc = base91(read[0]);
+	 primeiro_asc = base91(read[0]);                 
      fscanf(fp_txt, "%c", read);
-	 segundo_asc = base91(read[0]);
-	 original = (91)*(primeiro_asc) + (segundo_asc); /* Obtém número original */
-     printf("\noriginal: %d\n", original);
-	 fprintf(fp_bin, "%x\n", original);	/* Imprime o número no arquivo binário */
+	 segundo_asc = base91(read[0]);                  
+	 original = (91)*(primeiro_asc) + (segundo_asc); 
+	 fprintf(fp_bin, "%x\n", original);	             
 	}
 
 	fclose(fp_bin);
@@ -164,23 +169,38 @@ void decodeTxt()
 
 }
 
- //retorna o indice referente ao caracter do vetor alfabeto
+/*********************************************************
+* Função que recebe um inteiro e busca no vetor alfabeto *
+* o caracter referente ao índice indicado pelo inteiro   *
+* passado como argumento.								 *
+*********************************************************/
+
 char retorna_caracter(int index){
 	return alfabeto[index];
 }
 
-// Função que converte 13 bits para um numero inteiro
+/***********************************************
+* Função que recebe um vetor de caracteres com *
+* 13 bits e converte para um número inteiro.   *
+***********************************************/
+
 int bin2int(char read[]){ 
-	int inteiro=0, aux=1;
-	for(int i=12; i>=0; i--){
-		if(read[i]=='1')
+	int inteiro = 0, aux = 1;
+	for(int i = 12 ; i >= 0 ; i--){
+		if(read[i] == '1')
 			inteiro += aux;
-		aux*=2;
+		aux * = 2;
 	}	
 	return inteiro;
 }
 
-// Lê de 13 bits (até o fim do arquivo .bin), calcula y1 e y2, e pega no vetor alfabeto pra printar em um novo arquivo
+/***********************************************
+* Função que lê um arquivo binário, codifica a *
+* informação em números inteiros, converte em  *
+* caracteres da tabela ASCII Base91 e grava o  *
+* conteúdo codificado em um arquivo texto.	   *
+***********************************************/
+
 void codeBin(){ 
 	int y1, y2;
 	FILE *arqLe,*arqEs;
@@ -193,28 +213,29 @@ void codeBin(){
 	arqEs = fopen("saida.txt", "w+");
 
 	do{	
-		for(int i=0; i<13;i++)
-			fscanf(arqLe, "%c", &read[i]);
-		y1 = y2 = bin2int(read);
-		y1 /= 91;
-		y2 %= 91;
-		fprintf(arqEs, "%c ", retorna_caracter(y1));
-		fprintf(arqEs, "%c ", retorna_caracter(y2)); 
+		for(int i = 0; i < 13 ; i++)
+			fscanf(arqLe, "%c", &read[i]);				/* Leitura do caracter no arquivo binário */
+		y1 = y2 = bin2int(read);						/* As variáveis y1 e y2 recebem o caracter lido já convertido em inteiro */
+		y1 /= 91;										/* A variável y1 recebe o quociente da divisão do número inteiro */
+		y2 %= 91;										/* A variável y2 recebe o resto da divisão do número inteiro */
+		fprintf(arqEs, "%c ", retorna_caracter(y1));	/* O caracter codificado na variável y1 é escrito no arquivo texto */
+		fprintf(arqEs, "%c ", retorna_caracter(y2));    /* O caracter codificado na variável y1 é escrito no arquivo texto */
 	}while(!feof(arqLe));
+	
 	fclose(arqLe);
 	fclose(arqEs);
 	printf("Code executado com sucesso!!\n\n");
 }
 
-/****************************************
-Função de interface de usuário, apresenta 
-as opções de codificação e decodificação 
-de arquivo ou encerrar o programa.
-*****************************************/
+/********************************************
+* Função de interface de usuário, apresenta *
+* as opções de codificação e decodificação  *
+* de arquivo ou encerrar o programa.        *
+********************************************/
 
 void mainMenu(){
 
-	int inicio;
+	int inicio = 0;
 
 	do
 	{
@@ -243,9 +264,9 @@ void mainMenu(){
     }
 }
 
-/****************************
-Função principal do programa.
-*****************************/
+/********************************
+* Função principal do programa. *
+********************************/
 
 int main(int argc, char *argv[])
 {
@@ -257,8 +278,5 @@ int main(int argc, char *argv[])
         mainMenu();
 	}
 	
-	//TODO: DOCUMENTAÇÃO INTERNA
-
-	//system("PAUSE");
 	return 0;
 }
